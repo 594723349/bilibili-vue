d52d706b50b91efd40b18b84892aa2f25e58871b
1836388557 <1836388557@qq.com>
2024-01-11 19:57:55 +0800

    base config

---
+++ src/interface/request.ts
@@ -0,0 +1,13 @@
+/** 普通数据响应 */
+export interface ResDataStruct<T> {
+  /** 响应内容体 */
+  data: T
+  /** api响应信息 */
+  message: string
+  /** api响应编码 */
+  code: number
+  /** api接口返回是否成功 */
+  success: boolean
+  /** api接口查询数据库总数 */
+  total: number | string | null
+}
