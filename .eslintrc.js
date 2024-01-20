6446c06e19d8450948e28bab5d62f38a4a54b8aa
cxf <cxf@8531.cn>
2024-01-20 15:04:06 +0800

    fix: 修复代码自动格式化失败

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,19 @@
 module.exports = {
-  root: true,
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    'plugin:prettier/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-v-html': 0,
-    'vue/v-on-event-hyphenation': 0,
-    'vue/no-template-shadow': 0,
-    'vue/multi-word-component-names': 'off'
-  }
+	root: true,
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+	},
+	extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'vue/no-v-html': 0,
+		'vue/v-on-event-hyphenation': 0,
+		'vue/no-template-shadow': 0,
+		'vue/multi-word-component-names': 'off',
+	},
 }
-
-
